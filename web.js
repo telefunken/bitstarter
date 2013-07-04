b437965367e913f3cfa1a4abada4aa6760cda5b2
Sakke Wiik <sakke.wiik@gmail.com>
2013-07-04 13:01:38 +0000

    Prints from index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var data = fs.readFileSync("./index.html");
 var buffer = new Buffer(data, "utf-8")
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
