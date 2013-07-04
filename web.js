623553df771522539ec0e9b551385014cdc5810f
Sakke Wiik <sakke.wiik@gmail.com>
2013-07-04 12:58:46 +0000

    Reads index.html

--- web.js
+++ web.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("./index.html");
+var buffer = new Buffer(data, "utf-8")
 app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
